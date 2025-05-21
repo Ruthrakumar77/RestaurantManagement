0fd55d994d1d6c24f5f59b22bb013b560d33a683
Ruthrakumar77 <ruthrakumard0@gmail.com>
2025-05-21 13:58:42 +0530

    router

--- src/pages/FoodMenu.jsx
+++ src/pages/FoodMenu.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function FoodMenu() {
   return (
     <>
-    <div>FoodMenu</div>
-    <div>Idly Dosa Vada Sambar Chatni</div>
+      <div>FoodMenu</div>
+      <div>Idly Dosa Vada Sambar Chatni</div>
     </>
   )
 }
